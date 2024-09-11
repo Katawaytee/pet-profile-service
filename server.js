1d483103dbc8ac2b9500d4b4b2cb85cde2489fac
Katawaytee <ktriampo@gmail.com>
2024-09-11 17:11:54 +0700

    setup project

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require('express');
-const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./configs/db');
+
+dotenv.config({ path: './configs/.env' });
+
+connectDB();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
