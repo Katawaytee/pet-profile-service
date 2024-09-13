a78ae42b450cd31b56577cbf606c5dcdf6c43bbc
Katawaytee <ktriampo@gmail.com>
2024-09-13 23:40:00 +0700

    feat: getRandomPets

--- routes/pets.js
+++ routes/pets.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const { createPet, getPets, getPet } = require("../controllers/pets");
+const { createPet, getPets, getPet, getRandomPets } = require("../controllers/pets");
 
 const router = express.Router();
 
 router.route("/").post(createPet).get(getPets);
+
+router.route("/random").get(getRandomPets);
+
 router.route("/:id").get(getPet);
 
 router.route("/user/:userId/pets").get(getPets);
+
 
 module.exports = router;
