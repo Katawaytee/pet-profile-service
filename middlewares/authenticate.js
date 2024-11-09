18aa857322cd4e4c4d9eb8a6fd26c435c56c011f
koraphat <koraphat.lum@gmail.com>
2024-11-09 19:31:10 +0700

    change fprmat

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,36 +1,31 @@
-
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const authenticate = (req, res, next) => {
-
-  const bearer = req.headers['authorization']
+  const bearer = req.headers["authorization"];
 
   if (!bearer) {
     return res.status(401).json({
       code: 401,
-      msg: "Token not found"
-    })
+      msg: "Token not found",
+    });
   }
 
-  const token = bearer.split(" ")[1]
+  const token = bearer.split(" ")[1];
 
   jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, user) => {
-
     if (err) {
       return res.status(401).json({
         code: 401,
-        msg: "Invalid Token"
-      })
+        msg: "Invalid Token",
+      });
     }
 
     req.body.user = {
       ...user,
-    }
+    };
 
-    next()
-
-  })
-
-}
+    next();
+  });
+};
 
 module.exports = authenticate;
